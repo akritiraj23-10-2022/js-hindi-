bcdf2d2c41c0536783f76c172450f16eba9421ad
Akriti Raj <121313412+akritiraj23-10-2022@users.noreply.github.com>
2024-08-31 17:37:16 +0000

    opreater Data Type

--- Basic/conversion.js
+++ Basic/conversion.js
@@ -31,3 +31,43 @@ let somenumber =33
 let stringNumber = String(somenumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+//***************Operations*****************************
+
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+
+// 1. problem code solve
+
+let str1 = "hello"
+let str2 = "world"
+
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + "2" + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 =  num2  = num3 = 2+2
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
