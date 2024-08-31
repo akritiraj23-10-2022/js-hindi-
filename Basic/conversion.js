d7460aa30a52a2e87ef99d5b5011f6475cc4272d
Akriti Raj <121313412+akritiraj23-10-2022@users.noreply.github.com>
2024-08-31 16:00:54 +0000

    CONVERSION OF DATA TYPES

---
+++ Basic/conversion.js
@@ -0,0 +1,33 @@
+let score ="33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+//Boolean  Conversion
+let isLoggedIn =1
+
+let booleanIsLoggendIn =  Boolean(isLoggedIn)
+console.log(booleanIsLoggendIn);
+
+// 1=>true; 0 => false
+// "" => false
+// "akriti" => true
+
+// string Number
+
+let somenumber =33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
